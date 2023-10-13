17f8c8652f0485065e6aea0abcabc9df87204a96
Ritin George D <ritingeorge.d@gmail.com>
2023-10-14 04:31:14 +0530

    feat: [web-app] authentication system

---
+++ calculate-web-app/src/app/auth.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from 'src/typing/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  authenticatedUser: User | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  signup(username: string, password: string) {
+    return this.http.post('http://localhost:9000/api/v1/auth/register', {username, password}, {responseType: 'text'});
+  }
+
+  login(username: string, password: string) {
+    return this.http.post('http://localhost:9000/api/v1/auth/login', {username, password}, {responseType: 'text'});
+  }
+
+  logout() {
+    this.authenticatedUser = null;
+    localStorage.removeItem('user');
+  }
+
+  getAuth() {
+    return this.authenticatedUser;
+  }
+
+  setAuth(user: User) {
+    this.authenticatedUser = user;
+  }
+}
