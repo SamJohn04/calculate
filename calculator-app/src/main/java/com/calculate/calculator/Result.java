64275fa8ac4f167eb799b5f78a3113c7f5e67664
SamJohn04 <samuel.john.codes@gmail.com>
2023-10-18 11:19:58 +0530

    feat: calculator-app: trace logging implemented. Response param: traceLog

--- calculator-app/src/main/java/com/calculate/calculator/Result.java
+++ calculator-app/src/main/java/com/calculate/calculator/Result.java
@@ -1,16 +1,33 @@
 package com.calculate.calculator;
 
+import java.util.ArrayList;
+
 public class Result {
     private final double result;
+    private final TraceLogItem[] traceLog;
     public Result() {
         this.result = 0;
+        this.traceLog = new TraceLogItem[0];
     }
 
     public Result(double result) {
         this.result = result;
+        this.traceLog = new TraceLogItem[0];
+    }
+
+    public Result(double result, ArrayList<TraceLogItem> traceLog) {
+        this.result = result;
+        this.traceLog = new TraceLogItem[traceLog.size()];
+        for(int i = 0; i < traceLog.size(); i++) {
+            this.traceLog[i] = traceLog.get(i);
+        }
     }
 
     public double getResult() {
         return result;
+    }
+
+    public TraceLogItem[] getTraceLog() {
+        return traceLog;
     }
 }
